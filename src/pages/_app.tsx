87d38d371ec9461b20923caa2ec6574e7a85af23
NUKUS777 <dawletkenesbaev620@gmail.com>
2023-11-27 13:07:35 +0500

    ok

---
+++ src/pages/_app.tsx
@@ -0,0 +1,35 @@
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+
+import { api } from "~/utils/api";
+
+import "~/styles/globals.css";
+import Head from "next/head";
+import { SideNav } from "~/components/SideNav";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Head>
+        <title>Twitter Clone</title>
+        <meta
+          name="description"
+          content="This is a Twitter clone by Web Dev Simplified"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto flex items-start sm:pr-4">
+        <SideNav />
+        <div className="min-h-screen flex-grow border-x">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </SessionProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
